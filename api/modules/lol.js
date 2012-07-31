a201805b6c02fa5c0d700340dbd413f69e3e905b
Juan Pablo Canepa <jpcanepa@gmail.com>
2012-07-31 15:05:39 -0400

    Added more test code

--- api/modules/lol.js
+++ api/modules/lol.js
@@ -4,9 +4,18 @@ function lol (req, res, args, vars) {
 	res.send({method: 'ble'});
 };
 
+function bla (req, res, args, vars) {
+	res.send({'args':args, 'vars': vars});
+}
+
+function register(e, name, func) {
+	console.log('Module ' + module.id + ': registering ' + name);
+	e[name] = func;
+}
+
 exports.registerExports = function(e) {
-	console.log("Module lol registering function lol");
-	e.lol = lol;
+	register(e,'lol',lol)
+	register(e,'bla',bla)
 }
 
 
